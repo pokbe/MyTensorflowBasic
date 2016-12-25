5a08e62988aee9b86c626f1f9d299b6393a2aef1
sunbohit <a824834@163.com>
2016-12-25 21:28:09 +0800

    更新双向长短时模型

--- Neural_Networks/BiLSTM.py
+++ Neural_Networks/BiLSTM.py
@@ -12,3 +12,27 @@ num_step = 28
 num_hidden = 100
 num_class = 10
 
+input_feature = tf.placeholder(tf.float32,[None,num_step*num_input])
+input_feature_reshape = tf.reshape(input_feature,[-1,num_step,num_input])
+input_label = tf.placeholder(tf.float32,[None,num_class])
+
+weights = {
+	'out' : tf.Variable(tf.random_normal([2*num_hidden,num_class]))
+}
+biases = {
+	'out' : tf.Variable(tf.random_normal([num_class]))
+}
+
+def BiLSTM_model(input_raw, w, b):
+	#tf.nn.rnn_cell.BasicLSTMCell.__init__(num_units, forget_bias=1.0, input_size=None, state_is_tuple=True, activation=tanh)
+	#tf.nn.bidirectional_rnn(cell_fw, cell_bw, inputs, initial_state_fw=None, initial_state_bw=None, dtype=None, sequence_length=None, scope=None)
+	#inputs: A length T list of inputs, each a tensor of shape [batch_size, input_size], or a nested tuple of such elements.
+	input_list = tf.unpack(input_raw, axis=1)
+
+	fw_cell = tf.nn.rnn_cell.BasicLSTMCell(num_hidden, forget_bias=0.9)
+	bw_cell = tf.nn.rnn_cell.BasicLSTMCell(num_hidden, forget_bias=0.9)
+
+	outputs, output_state_fw, output_state_bw = tf.nn.bidirectional_rnn(fw_cell, bw_cell, input_list, dtype = tf.float32)
+	result = tf.add(tf.matmul(outputs, w['out']),b['out'])
+	return result
+
