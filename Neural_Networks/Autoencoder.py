5a22762f411b9bee1cf86e2321a4d7d5a1c753ab
sunbohit <a824834@163.com>
2016-12-27 15:10:49 +0800

    添加深度自编码图模型

--- Neural_Networks/Autoencoder.py
+++ Neural_Networks/Autoencoder.py
@@ -13,3 +13,32 @@ num_hidden_2 = 150
 
 input_feature = tf.placeholder(tf.float32,[None,num_input])
 
+weights = {
+	'encode_hidden_1' : tf.Variable(tf.random_normal([num_input,num_hidden_1])),
+	'encode_hidden_2' : tf.Variable(tf.random_normal([num_hidden_1,num_hidden_2])),
+	'decode_hidden_1' : tf.Variable(tf.random_normal([num_hidden_2,num_hidden_1])),
+	'decode_hidden_2' : tf.Variable(tf.random_normal([num_hidden_1,num_input]))
+}
+biases = {
+	'encode_hidden_1' : tf.Variable(tf.random_normal([num_hidden_1])),
+	'encode_hidden_2' : tf.Variable(tf.random_normal([num_hidden_2])),
+	'decode_hidden_1' : tf.Variable(tf.random_normal([num_hidden_1])),
+	'decode_hidden_2' : tf.Variable(tf.random_normal([num_input]))
+}
+
+def encoder_model(input_raw, w, b):
+	output_1 = tf.nn.sigmoid(tf.add(tf.matmul(input_raw,w['encode_hidden_1']), b['encode_hidden_1']))
+	output_2 = tf.nn.sigmoid(tf.add(tf.matmul(output_1,w['encode_hidden_2']), b['encode_hidden_2']))
+	return output_2
+
+def decoder_model(output_raw, w, b):
+	revert_1 = tf.nn.sigmoid(tf.add(tf.matmul(output_raw,w['decode_hidden_1']), b['decode_hidden_1']))
+	revert_2 = tf.nn.sigmoid(tf.add(tf.matmul(revert_1,w['decode_hidden_2']), b['decode_hidden_2']))
+	return revert_2
+
+encode_feature = encoder_model(input_feature, weights, biases)
+revert_feature = decoder_model(encode_feature, weights, biases)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(revert_feature, input_feature))
+optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+
